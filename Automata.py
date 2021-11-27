a4d86122af84c8760084077367b7e64e6c8c46ce
Piotr Wilkosz <pwilkosz99@gmail.com>
2021-11-28 00:42:18 +0100

    Data storage models RGB/NSWE

--- Automata.py
+++ Automata.py
@@ -1,9 +1,8 @@
 import numpy
 
-def generateMatrix(ImageMatrix, OldMatrix):
+def generateMatrix(ImageMatrix, StateMatrix):
     for x in range(400):
         for y in range(600): # fill with value only part of matrixes
             ImageMatrix[y][x] = [255, 0, 0]
-            OldMatrix[y][x] = [0, 255, 0]
 
-    return ImageMatrix, OldMatrix
+    return ImageMatrix, StateMatrix
