931f9791755120429bd7c8f2701620713bb85be1
Piotr Wilkosz <pwilkosz99@gmail.com>
2021-11-27 21:22:44 +0100

    Window creation

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from tkinter import *
+
+
 def main():
-    print("Let's start coding")
+    window = Tk()
+    window.configure(width=800, height=600)
+    window.title("Lattice Boltzman Automata by Piotr Wilkosz")
+    
+    window.mainloop()
 
 
 if __name__ == '__main__':
