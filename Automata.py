b7beee15f62c377b0f3185e524fef523b08cf79a
Piotr Wilkosz <pwilkosz99@gmail.com>
2021-11-28 00:03:59 +0100

    Image printing

--- Automata.py
+++ Automata.py
@@ -1,9 +1,9 @@
 import numpy
 
 def generateMatrix(ImageMatrix, OldMatrix):
-    for x in range(800):
-        for y in range(600): # fill with value only part of matrixes
-            ImageMatrix[x][y]=1
-            OldMatrix[x][y]=1
+    for x in range(600):
+        for y in range(300): # fill with value only part of matrixes
+            ImageMatrix[x][y] = [255, 0, 0]
+            OldMatrix[x][y] = [0, 255, 0]
 
-            return ImageMatrix, OldMatrix
+    return ImageMatrix, OldMatrix
