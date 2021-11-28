24be337e5b939b92d21fc5a3e5d6d5502c4b5a51
PWilkosz99L <pwilkosz99@gmail.com>
2021-11-28 16:13:03 +0100

    Wall logic

--- Automata.py
+++ Automata.py
@@ -9,9 +9,10 @@ def generateMatrix(ImageMatrix, StateMatrix):
     # Generate starting particles
     for x in range(294):
         for y in range(600):  # fill with value only part of matrixes
-            StateMatrix[y][x][0] = 1  # example
             if(randomByProbability(0.5)):  # 0-1
                 ImageMatrix[y][x] = [255, 0, 0]
+                # on the start each generated patricle on each cell flow in only one dirtection
+                StateMatrix[y][x][random.randint(0, 3)] = 1
             else:  # TODO repair weird triangle inside random image
                 ImageMatrix[x][y] = [0, 0, 0]
 
@@ -19,11 +20,11 @@ def generateMatrix(ImageMatrix, StateMatrix):
     for y in range(0, 250):
         for x in range(295, 305):
             ImageMatrix[y][x] = [255, 255, 255]
-            StateMatrix[y][x] = 5
+            StateMatrix[y][x][4] = 1
     for y in range(350, 600):
         for x in range(295, 305):
             ImageMatrix[y][x] = [255, 255, 255]
-            StateMatrix[y][x] = 5
+            StateMatrix[y][x][4] = 1
     return ImageMatrix, StateMatrix
 
 # 0:N 1:S 2:W 3:E 4:SOLID
@@ -49,10 +50,13 @@ def simulate(ImageMatrix, StateMatrix, newImageMatrix, newStateMatrx):
             #     if(StateMatrix[y][x][s] == 1):
             #         stateCouter += 1
             # if(stateCouter > 1):
+            if(StateMatrix[y][x][4] == 1): # wall stay on the same position and don't influence on other cells
+                newStateMatrx[y][x][4] = 1
+                newImageMatrix[y][x] = [255, 255, 255]
             if(StateMatrix[y][x][0] == 1):
-                if(y>=599):
+                if(y >= 599):
                     print("out of bound")
-                    #TODO reverse direction 
+                    #TODO reverse direction
                 else:
                     newStateMatrx[y+1][x][0] = 1
                     newImageMatrix[y+1][x] = [255, 0, 0]
