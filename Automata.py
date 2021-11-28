2d4a693c253f6fb90bd593a25bac3a8987adb890
PWilkosz99L <pwilkosz99@gmail.com>
2021-11-28 19:19:10 +0100

    Flow in all directions

--- Automata.py
+++ Automata.py
@@ -40,30 +40,53 @@ def transitionRule(ImageMatrix, StateMatrix, main, canvas):
     image = ImageTk.PhotoImage(image=Image.fromarray(newImageMatrix))
     canvas.image = image
     canvas.create_image(0, 0, anchor="nw", image=image)
-    main.after(10, lambda: transitionRule(newImageMatrix, newStateMatrx, main, canvas)) #recurensive
+    main.after(10, lambda: transitionRule(newImageMatrix,
+               newStateMatrx, main, canvas))  # recurensive
+
 
 def simulate(ImageMatrix, StateMatrix, newImageMatrix, newStateMatrx):
     for x in range(800):  # TODO automatic range
         for y in range(600):
             stateCouter = 0
-            #for s in range(5):
-            #     if(StateMatrix[y][x][s] == 1):
-            #         stateCouter += 1
-            # if(stateCouter > 1):
-            if(StateMatrix[y][x][4] == 1): # wall stay on the same position and don't influence on other cells
+            for s in range(5):
+                if(StateMatrix[y][x][s] == 1):
+                    stateCouter += 1
+            # wall stay on the same position and don't influence on other cells
+            if(StateMatrix[y][x][4] == 1):
                 newStateMatrx[y][x][4] = 1
                 newImageMatrix[y][x] = [255, 255, 255]
             else:
-                if(StateMatrix[y][x][0] == 1):
-                    if(y <= 1 or StateMatrix[y-1][x][4]==1):
+                if(StateMatrix[y][x][0] == 1):  # North
+                    if(y <= 1 or StateMatrix[y-1][x][4] == 1):
                         print("out of bound")
                         #TODO reverse direction
                     else:
                         newStateMatrx[y-1][x][0] = 1
                         newImageMatrix[y-1][x] = [255, 0, 0]
+                if(StateMatrix[y][x][1] == 1):  # South
+                    if(y >= 599 or StateMatrix[y+1][x][4] == 1):
+                        print("out of bound")
+                        #TODO reverse direction
+                    else:
+                        newStateMatrx[y+1][x][1] = 1
+                        newImageMatrix[y+1][x] = [255, 0, 0]
+                if(StateMatrix[y][x][2] == 1):  # West
+                    if(x <= 1 or StateMatrix[y][x-1][2] == 1):
+                        print("out of bound")
+                        #TODO reverse direction
+                    else:
+                        newStateMatrx[y][x-1][2] = 1
+                        newImageMatrix[y][x-1] = [255, 0, 0]
+                if(StateMatrix[y][x][3] == 1):  # East
+                    if(x >= 799 or StateMatrix[y][x+1][3] == 1):
+                        print("out of bound")
+                        #TODO reverse direction
+                    else:
+                        newStateMatrx[y][x+1][3] = 1
+                        newImageMatrix[y][x+1] = [255, 0, 0]
 
     return newImageMatrix, newStateMatrx
-    
+
 # 0 1 2 3 *
 # 1
 # 2
