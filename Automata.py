7dd319a261cfe0f1ff5b1f3127fdc91bb4e62117
PWilkosz99L <pwilkosz99@gmail.com>
2021-11-28 14:36:13 +0100

    Creation of base transition rule

--- Automata.py
+++ Automata.py
@@ -1,4 +1,7 @@
+from tkinter import Canvas
+import numpy
 from Utilities import *
+from PIL import ImageTk, Image
 
 
 def generateMatrix(ImageMatrix, StateMatrix):
@@ -22,3 +25,13 @@ def generateMatrix(ImageMatrix, StateMatrix):
             ImageMatrix[y][x] = [255, 255, 255]
             StateMatrix[y][x] = 5
     return ImageMatrix, StateMatrix
+
+def transitionRule(ImageMatrix, StateMatrix, canvas):
+    newImageMatrix = numpy.zeros([600, 800, 3], dtype=numpy.uint8) 
+    newStateMatrx = numpy.zeros([600, 800, 5], dtype=numpy.uint8) 
+    for x in range(800): #TODO automatic range
+        for y in range(600):
+            newImageMatrix[y][x] = [0, 255, 0]
+    image = ImageTk.PhotoImage(image=Image.fromarray(newImageMatrix))
+    canvas.image=image
+    canvas.create_image(0, 0, anchor="nw", image=image)
