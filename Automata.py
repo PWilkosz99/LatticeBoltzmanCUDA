38a886a003f361d4a7ba7b2e2e053414b25470c3
Piotr Wilkosz <pwilkosz99@gmail.com>
2021-11-30 22:09:50 +0100

    Race condition problem

--- Automata.py
+++ Automata.py
@@ -1,9 +1,11 @@
+from os import stat
 from tkinter import Canvas
 import numpy
 from Utilities import *
 from PIL import ImageTk, Image
 import numba
 from numba import prange
+
 
 @numba.jit(nopython=True, parallel=True)
 def generateMatrix(ImageMatrix, StateMatrix):
@@ -30,6 +32,8 @@ def generateMatrix(ImageMatrix, StateMatrix):
     return ImageMatrix, StateMatrix
 
 # 0:N 1:S 2:W 3:E 4:SOLID
+
+
 def transitionRule(ImageMatrix, StateMatrix, main, canvas):
     newImageMatrix = numpy.zeros(
         [ImageMatrix.shape[0], ImageMatrix.shape[1], 3], dtype=numpy.uint8)
@@ -45,59 +49,60 @@ def transitionRule(ImageMatrix, StateMatrix, main, canvas):
     canvas.image = image
     canvas.create_image(0, 0, anchor="nw", image=image)
     main.after(1, lambda: transitionRule(newImageMatrix,
-              newStateMatrx, main, canvas))  # recurensive
+                                         newStateMatrx, main, canvas))  # recurensive
+
 
 @numba.jit(nopython=True, parallel=True)
 def simulate(ImageMatrix, StateMatrix, newImageMatrix, newStateMatrx):
-    for x in prange(newImageMatrix.shape[1]):  # X:1 Y:2
+    for x in prange(newImageMatrix.shape[1]):  # X:1 Y:0
         for y in prange(newImageMatrix.shape[0]):
-            stateCouter = 0
-            for s in prange(5):
-                if(StateMatrix[y][x][s] == 1):
-                    stateCouter += 1
-            if((stateCouter == 2 and StateMatrix[y][x][0] == 1 and StateMatrix[y][x][1] == 1) or (stateCouter == 2 and StateMatrix[y][x][2] == 1 and StateMatrix[y][x][3] == 1)):
-                if(x >= 1 & y >= 1 & x <= newImageMatrix.shape[1]-1 & y <= newImageMatrix.shape[0]-1):
-                    if(StateMatrix[y][x][0] == 1 & StateMatrix[y][x][1] == 1):
-                        StateMatrix[y][x-1][2] = 1
-                        StateMatrix[y][x+1][3] = 1
+            stateTmp = StateMatrix[y][x]
+            if(stateTmp[4] != 1):
+                if(stateTmp[0]==1 and stateTmp[1]==1 and stateTmp[2]==0 and stateTmp[3]==0):
+                    if(x-1>=0):
+                        newStateMatrx[y][x-1][2] = 1
                         newImageMatrix[y][x-1] = [255, 0, 0]
-                        newImageMatrix[y][x+2] = [255, 0, 0]
-                    elif(StateMatrix[y][x][2] == 1 & StateMatrix[y][x][3] == 1):
-                        StateMatrix[y-1][x][0] = 1
-                        StateMatrix[y+1][x+1][1] = 1
-                        newImageMatrix[y-1][x] = [255, 0, 0]
+                    if(x+1<=newImageMatrix.shape[0]):
+                        newStateMatrx[y][x+1][3] = 1
+                        newImageMatrix[y][x+1] = [255, 0, 0]
+                elif(stateTmp[0]==0 and stateTmp[1]==0 and stateTmp[2]==1 and stateTmp[3]==1):
+                    if(y-1>=0):
+                        #newStateMatrx[y-1][x][0] = 1 #problem when program simultaneously save data to the same cell
+                        newImageMatrix[y-1][x] = [255, 0, 0] # TODO: rewrite the enitre method
+                    if(y+1<=newImageMatrix.shape[1]):
+                        newStateMatrx[y+1][x][1] = 1
                         newImageMatrix[y+1][x] = [255, 0, 0]
-                    #TODO : case when they bounce on 90'
-            else:
-                # wall stay on the same position and don't influence on other cells
-                if(StateMatrix[y][x][4] == 1):
-                    newStateMatrx[y][x][4] = 1
-                    newImageMatrix[y][x] = [255, 255, 255]
                 else:
-                    if(StateMatrix[y][x][0] == 1):  # North
+                    if(stateTmp[0] == 1):  # North
                         if(y <= 1 or StateMatrix[y-1][x][4] == 1):
                             newStateMatrx[y][x][1] = 1
                         else:
                             newStateMatrx[y-1][x][0] = 1
                             newImageMatrix[y-1][x] = [255, 0, 0]
-                    if(StateMatrix[y][x][1] == 1):  # South
+                    if(stateTmp[1] == 1):  # South
                         if(y >= newImageMatrix.shape[0]-1 or StateMatrix[y+1][x][4] == 1):
                             newStateMatrx[y][x][0] = 1
                         else:
                             newStateMatrx[y+1][x][1] = 1
                             newImageMatrix[y+1][x] = [255, 0, 0]
-                    if(StateMatrix[y][x][2] == 1):  # West
+                    if(stateTmp[2] == 1):  # West
                         if(x <= 1 or StateMatrix[y][x-1][2] == 1):
                             newStateMatrx[y][x][3] = 1
                         else:
                             newStateMatrx[y][x-1][2] = 1
                             newImageMatrix[y][x-1] = [255, 0, 0]
-                    if(StateMatrix[y][x][3] == 1):  # East
+                    if(stateTmp[3] == 1):  # East
                         if(x >= newImageMatrix.shape[1]-1 or StateMatrix[y][x+1][3] == 1):
                             newStateMatrx[y][x][2] = 1
                         else:
                             newStateMatrx[y][x+1][3] = 1
                             newImageMatrix[y][x+1] = [255, 0, 0]
+            else:  # wall stay on the same position and don't influence on other cells
+                newStateMatrx[y][x][4] = 1
+                newImageMatrix[y][x] = [255, 255, 255]
+
+
+#TODO : case when they bounce on 90'
 
     return newImageMatrix, newStateMatrx
 
