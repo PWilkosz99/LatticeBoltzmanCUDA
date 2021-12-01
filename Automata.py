f409fb262784ba928e7cdaa37126d45bb85ed3ce
PWilkosz99L <pwilkosz99@gmail.com>
2021-12-01 10:42:59 +0100

    Wall collision

--- Automata.py
+++ Automata.py
@@ -13,9 +13,9 @@ def generateMatrix(ImageMatrix, StateMatrix):
     # Generate starting particles
     for x in prange(294):
         for y in prange(600):  # fill with value only part of matrixes
-            if(x == 0 or y == 0 or x == 800 or y == 600):     # Generate wall around
-                ImageMatrix[y][x] = [255, 255, 255]
-                StateMatrix[y][x][4] = 1
+            # if(x == 1 or y ==1 or x == 799 or x == 550 or y == 599 or y == 500):     # Generate wall around
+            #     ImageMatrix[y][x] = [255, 255, 255]
+            #     StateMatrix[y][x][4] = 1
             if(random.random() < 0.5):  # 0-1
                 ImageMatrix[y][x] = [255, 0, 0]
                 # on the start each generated patricle on each cell flow in only one dirtection
@@ -32,6 +32,19 @@ def generateMatrix(ImageMatrix, StateMatrix):
         for x in prange(295, 305):
             ImageMatrix[y][x] = [255, 255, 255]
             StateMatrix[y][x][4] = 1
+
+
+    for y in prange(0, 600):
+            ImageMatrix[y][1] = [255, 255, 255]
+            StateMatrix[y][1][4] = 1         
+            ImageMatrix[y][799] = [255, 255, 255]
+            StateMatrix[y][798][4] = 1
+    for x in prange(0, 800):
+            ImageMatrix[1][x] = [255, 255, 255]
+            StateMatrix[1][x][4] = 1
+            ImageMatrix[598][x] = [255, 255, 255]
+            StateMatrix[598][x][4] = 1
+
     return ImageMatrix, StateMatrix
 
 # 0:N 1:S 2:W 3:E 4:SOLID
@@ -57,14 +70,18 @@ def transitionRule(ImageMatrix, StateMatrix, main, canvas):
 
 @numba.jit(nopython=True, parallel=True)
 def simulate(ImageMatrix, StateMatrix, newImageMatrix, newStateMatrx):
-    for x in prange(1, newImageMatrix.shape[1]-1):  # X:1 Y:0
-        for y in prange(1, newImageMatrix.shape[0]-1):
+    for x in prange(1, newImageMatrix.shape[1]):  # X:1 Y:0
+        for y in prange(1, newImageMatrix.shape[0]):
             stateTmp = StateMatrix[y][x]
             if(stateTmp[4] != 1):
                 stateN = StateMatrix[y-1][x][1]
                 stateS = StateMatrix[y+1][x][0]
                 stateW = StateMatrix[y][x-1][3]
                 stateE = StateMatrix[y][x+1][2]
+                stateSolidN = StateMatrix[y-1][x][4]
+                stateSolidS = StateMatrix[y+1][x][4]
+                stateSolidW = StateMatrix[y][x-1][4]
+                stateSolidE = StateMatrix[y][x+1][4]
 
                 if(stateN == 1 and stateS == 1 and stateW == 0 and stateE == 0):
                     newStateMatrx[y][x][2] = 1
@@ -77,18 +94,34 @@ def simulate(ImageMatrix, StateMatrix, newImageMatrix, newStateMatrx):
                     pass
                 else:
                     if(stateN == 1):
-                        newStateMatrx[y][x][1] = 1
-                        newImageMatrix[y][x] = [255, 0, 0]
+                        if(stateSolidS == 1):
+                            newStateMatrx[y][x][0] = 1
+                            newImageMatrix[y][x] = [255, 0, 0]
+                        else:
+                            newStateMatrx[y][x][1] = 1
+                            newImageMatrix[y][x] = [255, 0, 0]
                     if(stateS == 1):
-                        newStateMatrx[y][x][0] = 1
-                        newImageMatrix[y][x] = [255, 0, 0]
+                        if(stateSolidN == 1):
+                            newStateMatrx[y][x][1] = 1
+                            newImageMatrix[y][x] = [255, 0, 0]
+                        else:
+                            newStateMatrx[y][x][0] = 1
+                            newImageMatrix[y][x] = [255, 0, 0]
                     if(stateW == 1):
-                        newStateMatrx[y][x][3] = 1
-                        newImageMatrix[y][x] = [255, 0, 0]
+                        if(stateSolidE == 1):
+                            newStateMatrx[y][x][2] = 1
+                            newImageMatrix[y][x] = [255, 0, 0]
+                        else:
+                            newStateMatrx[y][x][3] = 1
+                            newImageMatrix[y][x] = [255, 0, 0]
                     if(stateE == 1):
-                        newStateMatrx[y][x][2] = 1
-                        newImageMatrix[y][x] = [255, 0, 0]
-
+                        if(stateSolidW == 1):
+                            newStateMatrx[y][x][3] = 1
+                            newImageMatrix[y][x] = [255, 0, 0]
+                        else:
+                            newStateMatrx[y][x][2] = 1
+                            newImageMatrix[y][x] = [255, 0, 0]
+                            
                     pass
             else:  # wall stay on the same position and don't influence on other cells
                 newStateMatrx[y][x][4] = 1
